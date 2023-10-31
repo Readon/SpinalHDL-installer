ed92bbcb6906ffc94cfe9a30815ae992d03b7b8d
Yindong <xydarcher@uestc.edu.cn>
2023-10-31 18:13:54 +0800

    test build by SpinalTemplate project.

---
+++ test_template.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+if [ $1 == "2.13" ]; then
+    _scala_version="2.13.6"
+else if [ $1 == "2.12" ]; then
+    _scala_version="2.12.13"
+else
+    _scala_version="2.11.12"
+fi
+_spinal_version=$2
+
+git clone https://github.com/SpinalHDL/SpinalTemplateSbt.git
+cd SpinalTemplateSbt
+git submodule update --init --recursive
+
+sed -i "s/\(val spinalVersion = \)\(\".*\"\)/\1\"${_spinal_version}\"/" build.sbt
+sed -i "s/\(scalaVersion := \)\(\".*\"\)/\1\"${_scala_version}\"/" build.sbt
+
+sbt compile
+sbt test
