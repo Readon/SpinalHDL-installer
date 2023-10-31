5d5ded602239e7f7fc467fa3620fe668f00d68aa
Xiao Yindong <xydarcher@uestc.edu.cn>
2023-10-31 23:48:30 +0800

    modify shell by shellcheck.net to fix the syntax error.

--- test_template.sh
+++ test_template.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-if [ $1 == "2.13" ]; then
+
+if [ "$1" == "2.13" ]; then
     _scala_version="2.13.6"
-else if [ $1 == "2.12" ]; then
+elif [ "$1" == "2.12" ]; then
     _scala_version="2.12.13"
 else
     _scala_version="2.11.12"
@@ -10,7 +11,7 @@ _spinal_version=$2
 
 do_test(){
     git clone https://github.com/SpinalHDL/SpinalTemplateSbt.git
-    cd SpinalTemplateSbt
+    cd SpinalTemplateSbt || exit
     git submodule update --init --recursive
 
     sed -i "s/\(val spinalVersion = \)\(\".*\"\)/\1\"${_spinal_version}\"/" build.sbt
