6b0bab076a2415446f670e6b2eab41e1f8020c88
Yindong <xydarcher@uestc.edu.cn>
2023-11-01 15:52:15 +0800

    specified sbt version and run it.

--- test_template.sh
+++ test_template.sh
@@ -9,7 +9,15 @@ else
 fi
 _spinal_version=$2
 
-do_test(){
+_sbt_version="1.8.0"
+_newmsys="/e/msys2-install-test"
+do_test(){    
+    export SDKMAN_DIR=${_newmsys}/sdkman
+    source $SDKMAN_DIR/bin/sdkman-init.sh
+    export COURSIER_CACHE=${_newmsys}/cache/.coursier
+    export SBT_OPTS="-Dsbt.version=${_sbt_version} -Dsbt.boot.directory=${_newmsys}/cache/.sbt/boot -Dsbt.global.base=${_newmsys}/cache/.sbt/1.0 -Dsbt.ivy.home=${_newmsys}/cache/.ivy2"
+    export SBT_CMD="${SDKMAN_DIR}/candidates/sbt/current/bin/sbt ${SBT_OPTS}"
+
     git clone https://github.com/SpinalHDL/SpinalTemplateSbt.git
     cd SpinalTemplateSbt || exit
     git submodule update --init --recursive
@@ -17,8 +25,8 @@ do_test(){
     sed -i "s/\(val spinalVersion = \)\(\".*\"\)/\1\"${_spinal_version}\"/" build.sbt
     sed -i "s/\(scalaVersion := \)\(\".*\"\)/\1\"${_scala_version}\"/" build.sbt
 
-    sbt -Dsbt.offline=true compile
-    sbt -Dsbt.offline=true test
+    ${SBT_CMD} -Dsbt.offline=true compile
+    ${SBT_CMD} -Dsbt.offline=true test
 }
 
 do_test
